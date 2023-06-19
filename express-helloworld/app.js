c102a8ee0526a4111e5da5adb7ab633765e3e853
Fran <fmfranco@viewnext.com>
2023-06-19 13:13:04 +0200

    añadimos a Fran

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -9,6 +9,10 @@ app.get('/mars', function(req, res) {
   res.send('Hello Mars!\n');
 });
 
+app.get('/fran', function(req, res) {
+  res.send('Hello Fran!\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
